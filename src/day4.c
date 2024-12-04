9e17398bb0f438b0df8bdc11d5fc7e445aee2f4c
jaburns <j@jaburns.net>
2024-12-04 10:40:40 -0700

    Replace macros with inline fns

--- src/day4.c
+++ src/day4.c
@@ -3,156 +3,149 @@
 #define DAY4_WIDTH  140
 #define DAY4_HEIGHT 140
 
+#define ROW_SIZE (DAY4_WIDTH + 1)
+#define XP1      1
+#define XP2      2
+#define XP3      3
+#define XM1      (-1)
+#define XM2      (-2)
+#define XM3      (-3)
+#define YP1      (1 * ROW_SIZE)
+#define YP2      (2 * ROW_SIZE)
+#define YP3      (3 * ROW_SIZE)
+#define YM1      (-YP1)
+#define YM2      (-YP2)
+#define YM3      (-YP3)
+
+internal void day4_check1(i64* count, char* input, i64 offset, i64 o1, i64 o2, i64 o3) {
+    if (input[offset + o1] == 'M' &&
+        input[offset + o2] == 'A' &&
+        input[offset + o3] == 'S') {
+        ++*count;
+    }
+}
+
+internal void day4_check2(i64* count, char* input, i64 offset) {
+    if (
+        input[offset] == 'A' &&
+        input[offset - 1 - ROW_SIZE] + input[offset + 1 + ROW_SIZE] == ((i64)('M' + 'S')) &&
+        input[offset + 1 - ROW_SIZE] + input[offset - 1 + ROW_SIZE] == ((i64)('M' + 'S'))
+    ) {
+        ++*count;
+    }
+}
+
 internal DayResult day4(Arena* arena, Str input) {
     i64 part1 = 0;
     i64 part2 = 0;
 
-#define Get(x, y) input.items[(x) + (y)]
-
-#define Check1(x1, y1, x2, y2, x3, y3)    \
-    if (Get(x + (x1), y + (y1)) == 'M' && \
-        Get(x + (x2), y + (y2)) == 'A' && \
-        Get(x + (x3), y + (y3)) == 'S') { \
-        part1++;                          \
-    }
-
-#define Check2(ch0, ch1, ch2, ch3)           \
-    if (Get(x - 1, y - row_size) == (ch0) && \
-        Get(x - 1, y + row_size) == (ch1) && \
-        Get(x + 1, y + row_size) == (ch2) && \
-        Get(x + 1, y - row_size) == (ch3)) { \
-        part2++;                             \
-    }
-
-    i64 row_size = DAY4_WIDTH + 1;
-    i64 y_max;
-
-    i64 p1 = row_size;
-    i64 p2 = 2 * p1;
-    i64 p3 = p2 + p1;
-    i64 n1 = -p1;
-    i64 n2 = -p2;
-    i64 n3 = -p3;
-
-    for (i64 y = 0, y_max = 3 * row_size; y < y_max; y += row_size) {
-        for (i64 x = 0; x < 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(1, p1, 2, p2, 3, p3);
-            }
+    for (i64 y = 0, y_max = 3 * ROW_SIZE; y < y_max; y += ROW_SIZE) {
+        for (i64 o = y, o_max = y + 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XP1 + YP1, XP2 + YP2, XP3 + YP3);
         }
-        for (i64 x = 3; x < DAY4_WIDTH - 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(1, p1, 2, p2, 3, p3);
-                Check1(-1, p1, -2, p2, -3, p3);
-            }
+        for (i64 o = y + 3, o_max = y + DAY4_WIDTH - 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XP1 + YP1, XP2 + YP2, XP3 + YP3);
+            day4_check1(&part1, input.items, o, XM1 + YP1, XM2 + YP2, XM3 + YP3);
         }
-        for (i64 x = DAY4_WIDTH - 3; x < DAY4_WIDTH; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(-1, p1, -2, p2, -3, p3);
-            }
+        for (i64 o = y + DAY4_WIDTH - 3, o_max = y + DAY4_WIDTH; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XM1 + YP1, XM2 + YP2, XM3 + YP3);
         }
     }
 
-    for (i64 y = 3 * row_size, y_max = (DAY4_HEIGHT - 3) * row_size; y < y_max; y += row_size) {
-        for (i64 x = 0; x < 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(1, p1, 2, p2, 3, p3);
-                Check1(1, n1, 2, n2, 3, n3);
-            }
-        }
-        for (i64 x = 3; x < DAY4_WIDTH - 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(1, p1, 2, p2, 3, p3);
-                Check1(-1, n1, -2, n2, -3, n3);
-                Check1(-1, p1, -2, p2, -3, p3);
-                Check1(1, n1, 2, n2, 3, n3);
-            } else if (Get(x, y) == 'A') {
-                Check2('M', 'M', 'S', 'S') else Check2('M', 'S', 'S', 'M') else Check2('S', 'S', 'M', 'M') else Check2('S', 'M', 'M', 'S')
-            }
-        }
-        for (i64 x = DAY4_WIDTH - 3; x < DAY4_WIDTH; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, p1, 0, p2, 0, p3);
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(-1, n1, -2, n2, -3, n3);
-                Check1(-1, p1, -2, p2, -3, p3);
-            }
-        }
-    }
-    for (i64 y = (DAY4_HEIGHT - 3) * row_size, y_max = DAY4_HEIGHT * row_size; y < y_max; y += row_size) {
-        for (i64 x = 0; x < 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(1, n1, 2, n2, 3, n3);
-            }
-        }
-        for (i64 x = 3; x < DAY4_WIDTH - 3; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(1, 0, 2, 0, 3, 0);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(-1, n1, -2, n2, -3, n3);
-                Check1(1, n1, 2, n2, 3, n3);
-            }
-        }
-        for (i64 x = DAY4_WIDTH - 3; x < DAY4_WIDTH; ++x) {
-            if (Get(x, y) == 'X') {
-                Check1(0, n1, 0, n2, 0, n3);
-                Check1(-1, 0, -2, 0, -3, 0);
-                Check1(-1, n1, -2, n2, -3, n3);
-            }
+    for (i64 y = ROW_SIZE, y_max = 3 * ROW_SIZE; y < y_max; y += ROW_SIZE) {
+        for (i64 o = y + 1, o_max = y + DAY4_WIDTH - 1; o < o_max; ++o) {
+            day4_check2(&part2, input.items, o);
         }
     }
 
-    for (i64 y = row_size, y_max = 3 * row_size; y < y_max; y += row_size) {
-        for (i64 x = 3; x < DAY4_WIDTH - 3; ++x) {
-            if (Get(x, y) == 'A') {
-                Check2('M', 'M', 'S', 'S') else Check2('M', 'S', 'S', 'M') else Check2('S', 'S', 'M', 'M') else Check2('S', 'M', 'M', 'S')
-            }
+    for (i64 y = 3 * ROW_SIZE, y_max = (DAY4_HEIGHT - 3) * ROW_SIZE; y < y_max; y += ROW_SIZE) {
+        for (i64 o = y, o_max = y + 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XP1 + YP1, XP2 + YP2, XP3 + YP3);
+            day4_check1(&part1, input.items, o, XP1 + YM1, XP2 + YM2, XP3 + YM3);
         }
-    }
-    for (i64 y = (DAY4_HEIGHT - 3) * row_size, y_max = (DAY4_HEIGHT - 1) * row_size; y < y_max; y += row_size) {
-        for (i64 x = 3; x < DAY4_WIDTH - 3; ++x) {
-            if (Get(x, y) == 'A') {
-                Check2('M', 'M', 'S', 'S') else Check2('M', 'S', 'S', 'M') else Check2('S', 'S', 'M', 'M') else Check2('S', 'M', 'M', 'S')
-            }
+        for (i64 o = y + 3, o_max = y + DAY4_WIDTH - 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XP1 + YP1, XP2 + YP2, XP3 + YP3);
+            day4_check1(&part1, input.items, o, XP1 + YM1, XP2 + YM2, XP3 + YM3);
+            day4_check1(&part1, input.items, o, XM1 + YP1, XM2 + YP2, XM3 + YP3);
+            day4_check1(&part1, input.items, o, XM1 + YM1, XM2 + YM2, XM3 + YM3);
         }
-    }
-    for (i64 y = row_size, y_max = (DAY4_HEIGHT - 1) * row_size; y < y_max; y += row_size) {
-        for (i64 x = 1; x < 3; ++x) {
-            if (Get(x, y) == 'A') {
-                Check2('M', 'M', 'S', 'S') else Check2('M', 'S', 'S', 'M') else Check2('S', 'S', 'M', 'M') else Check2('S', 'M', 'M', 'S')
-            }
+        for (i64 o = y + DAY4_WIDTH - 3, o_max = y + DAY4_WIDTH; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YP1, YP2, YP3);
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XM1 + YP1, XM2 + YP2, XM3 + YP3);
+            day4_check1(&part1, input.items, o, XM1 + YM1, XM2 + YM2, XM3 + YM3);
         }
-        for (i64 x = DAY4_WIDTH - 3; x < DAY4_WIDTH - 1; ++x) {
-            if (Get(x, y) == 'A') {
-                Check2('M', 'M', 'S', 'S') else Check2('M', 'S', 'S', 'M') else Check2('S', 'S', 'M', 'M') else Check2('S', 'M', 'M', 'S')
-            }
+        for (i64 o = y + 1, o_max = y + DAY4_WIDTH - 1; o < o_max; ++o) {
+            day4_check2(&part2, input.items, o);
         }
     }
 
-#undef Get
-#undef Check1
-#undef Check2
+    for (i64 y = (DAY4_HEIGHT - 3) * ROW_SIZE, y_max = (DAY4_HEIGHT - 1) * ROW_SIZE; y < y_max; y += ROW_SIZE) {
+        for (i64 o = y + 1, o_max = y + DAY4_WIDTH - 1; o < o_max; ++o) {
+            day4_check2(&part2, input.items, o);
+        }
+    }
+
+    for (i64 y = (DAY4_HEIGHT - 3) * ROW_SIZE, y_max = DAY4_HEIGHT * ROW_SIZE; y < y_max; y += ROW_SIZE) {
+        for (i64 o = y, o_max = y + 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XP1 + YM1, XP2 + YM2, XP3 + YM3);
+        }
+        for (i64 o = y + 3, o_max = y + DAY4_WIDTH - 3; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XP1, XP2, XP3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XP1 + YM1, XP2 + YM2, XP3 + YM3);
+            day4_check1(&part1, input.items, o, XM1 + YM1, XM2 + YM2, XM3 + YM3);
+        }
+        for (i64 o = y + DAY4_WIDTH - 3, o_max = y + DAY4_WIDTH; o < o_max; ++o) {
+            if (input.items[o] != 'X') continue;
+            day4_check1(&part1, input.items, o, YM1, YM2, YM3);
+            day4_check1(&part1, input.items, o, XM1, XM2, XM3);
+            day4_check1(&part1, input.items, o, XM1 + YM1, XM2 + YM2, XM3 + YM3);
+        }
+    }
 
     DayResult result       = {0};
     result.parts[0].as_i64 = part1;
     result.parts[1].as_i64 = part2;
     return result;
 }
+
+#undef ROW_SIZE
+#undef XP1
+#undef XP2
+#undef XP3
+#undef XM1
+#undef XM2
+#undef XM3
+#undef YP1
+#undef YP2
+#undef YP3
+#undef YM1
+#undef YM2
+#undef YM3
