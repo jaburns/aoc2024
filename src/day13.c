dec3e868543668bf97a7790e4ea8712b4194ea3a
jaburns <j@jaburns.net>
2024-12-13 13:13:24 -0700

    Simpler 13

--- src/day13.c
+++ src/day13.c
@@ -1,61 +1,47 @@
 #include "main.h"
 
-structdef(Day13Machine) {
-    f64 ax, ay;
-    f64 bx, by;
-    f64 cx, cy;
-};
-DefArrayTypes(Day13Machine);
+internal u64 day13_solve(f64 ax, f64 ay, f64 bx, f64 by, f64 cx, f64 cy, f64 offset) {
+    f64 px = cx + offset;
+    f64 py = cy + offset;
 
-internal u64 day13_solve(Day13Machine* m, f64 offset) {
-    f64 px = m->cx + offset;
-    f64 py = m->cy + offset;
-
-    f64 fy = (px / m->ax * m->ay - py) / (m->ay * m->bx / m->ax - m->by);
-    f64 fx = px / m->ax - m->bx / m->ax * fy;
+    f64 fy = (px / ax * ay - py) / (bx / ax * ay - by);
+    f64 fx = (px - bx * fy) / ax;
 
     f64 x = round(fx);
     f64 y = round(fy);
 
-    if (x * m->ax + y * m->bx == px && x * m->ay + y * m->by == py) {
-        return 3 * (u64)x + (u64)y;
-    }
-    return 0;
+    return x * ax + y * bx == px && x * ay + y * by == py
+               ? 3 * (u64)x + (u64)y
+               : 0;
 }
 
 internal DayResult day13(Arena* arena, Str input) {
     u64 part1 = 0;
     u64 part2 = 0;
 
-    Vec_Day13Machine machines = VecAlloc(Day13Machine, arena, 1024);
-
     StrSplitIter lines_iter = StrSplitIter_new('\n', input);
     while (!lines_iter.done) {
-        Day13Machine* machine = VecPush(machines);
-
-        Str ax      = str_before_first_index(',', str_after_first_index('+', lines_iter.item));
-        Str ay      = str_after_last_index('+', lines_iter.item);
-        machine->ax = (f64)str_parse_u32(ax, 10);
-        machine->ay = (f64)str_parse_u32(ay, 10);
+        Str sax = str_before_first_index(',', str_after_first_index('+', lines_iter.item));
+        Str say = str_after_last_index('+', lines_iter.item);
+        f64 ax  = (f64)str_parse_u32(sax, 10);
+        f64 ay  = (f64)str_parse_u32(say, 10);
         StrSplitIter_next(&lines_iter);
 
-        Str bx      = str_before_first_index(',', str_after_first_index('+', lines_iter.item));
-        Str by      = str_after_last_index('+', lines_iter.item);
-        machine->bx = (f64)str_parse_u32(bx, 10);
-        machine->by = (f64)str_parse_u32(by, 10);
+        Str sbx = str_before_first_index(',', str_after_first_index('+', lines_iter.item));
+        Str sby = str_after_last_index('+', lines_iter.item);
+        f64 bx  = (f64)str_parse_u32(sbx, 10);
+        f64 by  = (f64)str_parse_u32(sby, 10);
         StrSplitIter_next(&lines_iter);
 
-        Str cx      = str_before_first_index(',', str_after_first_index('=', lines_iter.item));
-        Str cy      = str_after_last_index('=', lines_iter.item);
-        machine->cx = (f64)str_parse_u32(cx, 10);
-        machine->cy = (f64)str_parse_u32(cy, 10);
+        Str scx = str_before_first_index(',', str_after_first_index('=', lines_iter.item));
+        Str scy = str_after_last_index('=', lines_iter.item);
+        f64 cx  = (f64)str_parse_u32(scx, 10);
+        f64 cy  = (f64)str_parse_u32(scy, 10);
         StrSplitIter_next(&lines_iter);
         StrSplitIter_next(&lines_iter);
-    }
 
-    for (u32 i = 0; i < machines.count; ++i) {
-        part1 += day13_solve(&machines.items[i], 0.0);
-        part2 += day13_solve(&machines.items[i], 10000000000000.0);
+        part1 += day13_solve(ax, ay, bx, by, cx, cy, 0.0);
+        part2 += day13_solve(ax, ay, bx, by, cx, cy, 10000000000000.0);
     }
 
     DayResult result       = {0};
