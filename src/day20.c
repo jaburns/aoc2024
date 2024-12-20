f8c5fb818f1b9e203e904954b25215914bdf19fe
jaburns <j@jaburns.net>
2024-12-20 12:47:27 -0700

    Much faster day 20

--- src/day20.c
+++ src/day20.c
@@ -12,75 +12,83 @@ internal DayResult day20(Arena* arena, Str input) {
     u32*      grid = ArrayAlloc(u32, arena, DAY20_SIZE * DAY20_SIZE);
     Vec_ivec2 path = VecAlloc(ivec2, arena, DAY20_SIZE * DAY20_SIZE);
 
-    ivec2 start, end;
-
-    u32 i = 0;
-    for (u32 y = 0; y < DAY20_SIZE; ++y) {
-        for (u32 x = 0; x < DAY20_SIZE; ++x) {
-            switch (input.items[i++]) {
-                case '.':
-                    grid[x + y * DAY20_SIZE] = UINT32_MAX;
-                    break;
-                case 'S':
-                    grid[x + y * DAY20_SIZE] = UINT32_MAX;
-                    start                    = ivec2(x, y);
-                    break;
-                case 'E':
-                    grid[x + y * DAY20_SIZE] = UINT32_MAX;
-                    end                      = ivec2(x, y);
-                    break;
+    {
+        ivec2 start, end;
+        u32   i = 0;
+        for (u32 y = 0; y < DAY20_SIZE; ++y) {
+            for (u32 x = 0; x < DAY20_SIZE; ++x) {
+                switch (input.items[i++]) {
+                    case '.':
+                        grid[x + y * DAY20_SIZE] = UINT32_MAX;
+                        break;
+                    case 'S':
+                        grid[x + y * DAY20_SIZE] = UINT32_MAX;
+                        start                    = ivec2(x, y);
+                        break;
+                    case 'E':
+                        grid[x + y * DAY20_SIZE] = UINT32_MAX;
+                        end                      = ivec2(x, y);
+                        break;
+                }
             }
+            i++;
         }
-        i++;
-    }
 
-    ivec2 pos  = start;
-    u32   step = 1;
-    for (;;) {
-        grid[pos.x + pos.y * DAY20_SIZE] = step++;
-        *VecPush(path)                   = pos;
-        if (ivec2_eq(pos, end)) break;
+        ivec2 pos  = start;
+        u32   step = 1;
+        for (;;) {
+            grid[pos.x + pos.y * DAY20_SIZE] = step++;
+            *VecPush(path)                   = pos;
+            if (ivec2_eq(pos, end)) break;
 
-        if (grid[pos.x + 1 + pos.y * DAY20_SIZE] == UINT32_MAX) {
-            pos.x++;
-            continue;
+            if (grid[pos.x + 1 + pos.y * DAY20_SIZE] == UINT32_MAX) {
+                pos.x++;
+                continue;
+            }
+            if (grid[pos.x - 1 + pos.y * DAY20_SIZE] == UINT32_MAX) {
+                pos.x--;
+                continue;
+            }
+            if (grid[pos.x + (pos.y + 1) * DAY20_SIZE] == UINT32_MAX) {
+                pos.y++;
+                continue;
+            }
+            if (grid[pos.x + (pos.y - 1) * DAY20_SIZE] == UINT32_MAX) {
+                pos.y--;
+                continue;
+            }
+            AssertUnreachable();
         }
-        if (grid[pos.x - 1 + pos.y * DAY20_SIZE] == UINT32_MAX) {
-            pos.x--;
-            continue;
-        }
-        if (grid[pos.x + (pos.y + 1) * DAY20_SIZE] == UINT32_MAX) {
-            pos.y++;
-            continue;
-        }
-        if (grid[pos.x + (pos.y - 1) * DAY20_SIZE] == UINT32_MAX) {
-            pos.y--;
-            continue;
-        }
-        AssertUnreachable();
     }
 
     u32 part1 = 0;
     u32 part2 = 0;
     for (u32 i = 0; i < path.count; ++i) {
-        ivec2 p   = path.items[i];
-        i32   cur = grid[p.x + p.y * DAY20_SIZE];
+        ivec2 pos = path.items[i];
+        i32   cur = grid[pos.x + pos.y * DAY20_SIZE];
 
-        ivec2 delta;
-        for (delta.y = -20; delta.y <= 20; ++delta.y) {
-            i32 xrange = 20 - Abs(delta.y);
-            for (delta.x = -xrange; delta.x <= xrange; ++delta.x) {
-                ivec2 p1 = ivec2_add(p, delta);
+        i32 miny = Max(1, pos.y - 20);
+        i32 maxy = Min(DAY20_SIZE - 2, pos.y + 20);
+        i32 dy   = miny - pos.y;
 
-                if (p1.x < 1 || p1.y < 1 || p1.x > DAY20_SIZE - 2 || p1.y > DAY20_SIZE - 2) continue;
+        for (i32 y = miny; y <= maxy; ++y) {
+            i32 abs_dy = Abs(dy);
+            i32 xrange = 20 - abs_dy;
+            i32 minx   = Max(1, pos.x - xrange);
+            i32 maxx   = Min(DAY20_SIZE - 2, pos.x + xrange);
+            i32 dx     = minx - pos.x;
 
-                i32  skip_len = ivec2_manhattan(delta);
-                i32  other    = grid[p1.x + p1.y * DAY20_SIZE];
+            for (i32 x = minx; x <= maxx; ++x) {
+                i32  skip_len = Abs(dx) + abs_dy;
+                i32  other    = grid[x + y * DAY20_SIZE];
                 bool count    = other - cur >= DAY20_MIN_SAVING + skip_len;
 
                 part1 += skip_len == 2 && count;
                 part2 += count;
+
+                ++dx;
             }
+            ++dy;
         }
     }
 
